12853f06ddbefd5111f217fd7205630dc3ffc613
Karanam Rama Chaitanya (Data Analytics/Hosur/TVSMotor) <karanam.rama@ISMAC0035.local>
2021-07-28 15:08:50 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='predicting houses price ususing various regression techniques/algos',
+    author='rama_chaitanya',
+    license='MIT',
+)
